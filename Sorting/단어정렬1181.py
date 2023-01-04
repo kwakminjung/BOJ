6d6b85cf9f353b1d73a006bfde7c67e1c626a693
KwakMJ <kwak1126mj@gmail.com>
2023-01-04 23:32:46 +0900

    Update 단어정렬1181.py

--- Sorting/단어정렬1181.py
+++ Sorting/단어정렬1181.py
@@ -3,11 +3,11 @@ def get_key(tup):
     return tup[1]
 N = int(sys.stdin.readline().strip())
 str_dic = {}
-str_val = ''
+str_key = ''
 for i in range(N):
-    str_val = sys.stdin.readline().strip()
-    str_dic[str_val] = len(str_val)
-str_dic = sorted(str_dic.items())
-str_dic.sort(key=get_key)
-for i in range(len(str_dic)):
-    print(str_dic[i][0])
+    str_key = sys.stdin.readline().strip()
+    str_dic[str_key] = len(str_key)
+str_dic = dict(sorted(str_dic.items()))
+str_dic = dict(sorted(str_dic.items(),key=lambda x:x[1]))
+for key in str_dic:
+    print('{}'.format(key))
