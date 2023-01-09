40479fd2d92a3999789148d2fa40cce5d5e4fe4e
KwakMJ <kwak1126mj@gmail.com>
2023-01-10 00:19:00 +0900

    Update 트ㅏㅊ;4378.py

--- AdHoc/트ㅏㅊ;4378.py
+++ AdHoc/트ㅏㅊ;4378.py
@@ -1,19 +1,22 @@
 import sys
 
 key_board = '`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;\'ZXCVBNM,./'
-
+def error_fix(letter):
+    temp_idx = key_board.index(letter)
+    return key_board[temp_idx-1]
 while 1:
-    error_key = sys.stdin.readline().strip()
-
-    if error_key == '':
+    error_key = []
+    error_key = sys.stdin.readline().rsplit()
+    if len(error_key) == 0:
         break
-    else:
-        result = ''
-        for i in range(len(error_key)):
-            if error_key[i] != ' ':
-                temp = error_key[i]
-                key_idx = key_board.index(temp)
-                result += key_board[key_idx-1]
-            else:
-                result += ' '
-    print(result)
+
+    ans_word = ''
+    ans_list = []
+    for word in error_key:
+        word_list = list(word)
+        for i in range(len(word)):
+            word_list[i] = error_fix(word_list[i])
+        ans_word =  ''.join(word_list)
+        ans_list.append(ans_word)
+    ans_str = ' '.join(ans_list)
+    print(ans_str)
