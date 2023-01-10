7ab9c18b5c353691a0457f3204d7d55c95a68c0a
KwakMJ <kwak1126mj@gmail.com>
2023-01-10 23:23:53 +0900

    Create 그룹단어체커1316.py

---
+++ Implement/그룹단어체커1316.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+group_word = 0
+for i in range(N):
+    word = input()
+    error = 0
+    for index in range(len(word)-1):  
+        if word[index] != word[index+1]:  
+            new_word = word[index+1:]  
+            if new_word.count(word[index]) > 0:  
+                error += 1 
+    if error == 0:
+        group_word += 1  
+print(group_word)
