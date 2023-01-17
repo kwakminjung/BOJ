d7d6f793a4c6ff311d28055097e15d4d0bb50180
KwakMJ <kwak1126mj@gmail.com>
2023-01-17 14:13:06 +0900

    Update 배부른마라토너10546.py

--- Hash/배부른마라토너10546.py
+++ Hash/배부른마라토너10546.py
@@ -1,17 +1,17 @@
 import sys
 N = int(sys.stdin.readline().rstrip())
-mrth_arr = dict()
+mrth_dic = dict()
 for _ in range(N):
     name = sys.stdin.readline().rstrip()
-    if name not in mrth_arr.keys():
-        mrth_arr[name] = 1
+    if name not in mrth_dic.keys():
+        mrth_dic[name] = 1
     else:
-        mrth_arr[name] += 1
+        mrth_dic[name] += 1
 for _ in range(N-1):
     com = sys.stdin.readline().rstrip()
-    mrth_arr[com] -= 1
+    mrth_dic[com] -= 1
 
-for key, value in mrth_arr.items():
+for key, value in mrth_dic.items():
     if value == 1:
         print(key)
         break
