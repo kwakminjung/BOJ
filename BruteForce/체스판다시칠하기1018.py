0f55629ae5362061ac6741e6d0269909aa841ef1
KwakMJ <kwak1126mj@gmail.com>
2023-01-25 16:47:41 +0900

    Update 체스판다시칠하기1018.py

--- BruteForce/체스판다시칠하기1018.py
+++ BruteForce/체스판다시칠하기1018.py
@@ -1,24 +1,30 @@
 import sys
-N, M  = map(int, sys.stdin.readline().rstrip().split())
-original = []
-count = []
+N, M = map(int, sys.stdin.readline().rstrip().split())
+board = []
+result = []
+ 
 for _ in range(N):
-    original.append(input())
-for a in range(N-7):
-    for b in range(M-7):
-        index1 = 0
-        index2 = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
-                        index1 += 1
-                    if original[i][j] != 'B':
-                        index2 += 1
+    board.append(sys.stdin.readline().rstrip())
+ 
+for i in range(N-7):
+    for j in range(M-7):
+        black = 0
+        white = 0
+ 
+        for a in range(i, i+8):
+            for b in range(j, j+8):
+                if (a + b) % 2 == 0:
+                    if board[a][b] != 'B':
+                        black += 1
+                    if board[a][b] != 'W':
+                        white += 1
                 else:
-                    if original[i][j] != 'B':
-                        index1 += 1
-                    if original[i][j] != 'W':
-                        index2 += 1
-        count.append(min(index1, index2))
-print(min(count))
+                    if board[a][b] != 'W':
+                        black += 1
+                    if board[a][b] != 'B':
+                        white += 1
+ 
+        result.append(black)
+        result.append(white)
+ 
+print(min(result))
