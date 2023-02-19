221abcfd96cd0f97114fec1e7912ac660512ea5d
KwakMJ <kwak1126mj@gmail.com>
2023-02-19 21:42:32 +0900

    [실버2] 상자넣기 1965번 : 다이나믹 프로그래밍

---
+++ DynamicProgramming/상자넣기.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+N = int(input().rstrip())
+box = list(map(int, input().rstrip().split()))
+dp = [1] * N
+for i in range(1, N):
+    for j in range(i):
+        if box[i] > box[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
