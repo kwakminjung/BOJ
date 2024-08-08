a80fc6a29bdbdce4336b7f4d322275803a4e4063
KwakMJ <kwak1126mj@gmail.com>
2024-08-09 03:52:11 +0900

    ♻️refactor: [골드5] 전구 상태 바꾸기 30023번 : 구현, 그리디 알고리즘
    회고
    - https://cuckoobird.tistory.com/196

--- GreedyAlgorithm/전구 상태 바꾸기30023.java
+++ GreedyAlgorithm/전구 상태 바꾸기30023.java
@@ -1,97 +1,69 @@
 /*
 백준 30023번 : 전구 상태 바꾸기 (골드5)
 알고리즘 분류 : 구현, 그리디 알고리즘
-풀이 : 
-결과 : 메모리초과
+풀이 : https://cuckoobird.tistory.com/196
+결과 : 맞았습니다 (메모리 16572KB | 시간 156ms | 코드 길이 1529B)
 */
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static class Node {
-        String light;
-        int cnt;
-
-        Node(String light, int cnt) {
-            this.light = light;
-            this.cnt = cnt;
-        }
-    }
-    
+	final static int MAX = 900_000;
+	
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        
-        n = Integer.parseInt(st.nextToken());
-        light = br.readLine();
-        origin = light;
-        
-        BFS(light, 0);
-        
-        System.out.println(ans - 1);
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		char[] lights = br.readLine().toCharArray();
+		
+		int firstLight = lightsOn(n, lights);
+		change(lights, 1);
+		int secondLight = lightsOn(n, lights) + 1;
+		change(lights, 1);
+		int thirdLigth = lightsOn(n, lights) + 2;
+		int minLights = Math.min(firstLight, Math.min(secondLight, thirdLigth));
+		int ans = (minLights == MAX) ? -1:minLights;
+		
+		System.out.println(ans);
+	}
+    
+    public static int lightsOn(int n, char[] lights) {
+    	char[] lightsCopy = Arrays.copyOf(lights, n);
+    	int cnt = 0;
+    	
+    	for(int i=2; i<n-1; i++) {
+    		while(lightsCopy[0] != lightsCopy[i-1]) {
+    			change(lightsCopy, i);
+    			cnt++;
+    		}
+    	}
+    	
+    	for(int i=1; i<n; i++) {
+    		if(lightsCopy[0] != lightsCopy[i]) {
+    			cnt = MAX;
+    			break;
+    		}
+    	}
+    	
+    	return cnt;
     }
     
-    static String origin;
-    static String light;
-    static int ans = 0;
-    static int n;
-    
-    public static void BFS(String pre, int cnt) {
-        Queue<Node> q = new LinkedList<>();
-        Set<String> visited = new HashSet<>();
-        q.add(new Node(pre, cnt));
-        visited.add(pre);
-        
-        while (!q.isEmpty()) {
-            Node current = q.poll();
-            String light = current.light;
-            cnt = current.cnt + 1;
-            
-            boolean allSame = true;
-            for (int i = 0; i < light.length() - 1; i++) {
-                if (light.charAt(i) != light.charAt(i + 1)) {
-                    allSame = false;
-                    break;
-                }
-            }
-
-            if (allSame) {
-                ans = cnt;
-                return;
-            }
-            
-            for (int i = 0; i <= light.length() - 3; i++) {
-                String next = change(light, i);
-                if (!visited.contains(next)) {
-                    q.add(new Node(next, cnt));
-                    visited.add(next);
-                }
-            }
-        }
-    }
-    
-    public static String change(String light, int idx) {
-        char[] arr = light.toCharArray();
-        for (int i = idx; i < idx + 3; i++) {
-            switch (arr[i]) {
-                case 'R':
-                    arr[i] = 'G';
-                    break;
-                case 'G':
-                    arr[i] = 'B';
-                    break;
-                case 'B':
-                    arr[i] = 'R';
-                    break;
-            }
-        }
-        return new String(arr);
+    public static void change(char[] lightsCopy, int idx) {
+    	for(int i=idx-1; i<=idx+1; i++) {
+    		switch(lightsCopy[i]) {
+    		case 'R':
+    			lightsCopy[i] = 'G';
+    			break;
+    		case 'G':
+    			lightsCopy[i] = 'B';
+    			break;
+    		case 'B':
+    			lightsCopy[i] = 'R';
+    			break;
+    		}
+    	}
     }
 }
