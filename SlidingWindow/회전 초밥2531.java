a78342151dcf8806e9f00c58cc91f8292c9cfb3d
KwakMJ <kwak1126mj@gmail.com>
2024-08-12 01:36:07 +0900

    ♻️refactor: [실버1] 회전 초밥 2531번 : 브루트포스 알고리즘, 두 포인터, 슬라이딩 윈도우
    백준 2531번 : 회전 초밥 (실버1)
    알고리즘 분류 : 브루트포스 알고리즘, 두 포인터, 슬라이딩 윈도우
    풀이 : https://cuckoobird.tistory.com/198
    결과 : 맞았습니다!! (메모리 31764KB | 시간 1224ms | 코드 길이 1641B)

--- SlidingWindow/회전 초밥2531.java
+++ SlidingWindow/회전 초밥2531.java
@@ -1,21 +1,18 @@
 /*
 백준 2531번 : 회전 초밥 (실버1)
 알고리즘 분류 : 브루트포스 알고리즘, 두 포인터, 슬라이딩 윈도우
-풀이 : 
-결과 : 틀렸습니다
+풀이 : https://cuckoobird.tistory.com/198
+결과 : 맞았습니다!! (메모리 31764KB | 시간 1224ms | 코드 길이 1641B)
 */
-
-package boj;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
-public class b2531 {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
@@ -27,40 +24,51 @@ public class b2531 {
 		c = Integer.valueOf(st.nextToken());
 		
 		int[] sushi = new int[n];
-		int[] flavor = new int[d];
-		int ans = 0;
+		Map<Integer, Integer> eated = new HashMap<Integer, Integer>();
+		for(int i=1; i<=d; i++) eated.put(i, 0);
 		for(int i=0; i<n; i++) {
 			sushi[i] = Integer.valueOf(br.readLine());
-			flavor[sushi[i]-1]++;
-			if(flavor[sushi[i]-1] == 1) ans ++;
 		}
-		if(sushi.length >= k) {
-			ans = solution(sushi, flavor, n, k, c);
-		}
+		
+		int ans = solution(sushi, eated, n, k, c);
 		System.out.println(ans);
 	}
 	
-	public static int solution(int[] sushi, int[] flavor, int n, int k, int c) {
+	public static int solution(int[] sushi, Map<Integer, Integer> eated, int n, int k, int c) {
 		int left = 0, right = k-1;
 		int ans = 0;
-		Set<Integer> set = new HashSet<>();
+		
 		
 		for(int i=0; i<k ;i++) {
-			set.add(sushi[i]);
+			eated.put(sushi[i], eated.get(sushi[i]) + 1);
 		}
-		set.add(c);
-		if(ans < set.size()) ans = set.size();
+		if(eated.get(c) == 0) eated.put(c, 1);
 		
-		for(int i=0; i<n; i++) {
-			left++;
-			if(flavor[sushi[left-1]-1] <= 1) set.remove(sushi[left-1]);
+		int cnt = 0;
+		for(Integer key : eated.keySet()){
+			if(eated.get(key) >= 1) {
+				cnt ++;
+			}
+		}
+		if(ans < cnt) ans = cnt;
+		
+		for(int i=0; i<n-1; i++) {
+			cnt = 0;
+			left ++;
+			right ++;
+			eated.put(sushi[left-1], eated.get(sushi[left-1]) - 1);
+			eated.put(sushi[right%n], eated.get(sushi[right%n]) + 1);
 			
-			right++;
-			set.add(sushi[right%n]);
-			set.add(c);
+			eated.put(c, eated.get(c) + 1);
 			
-			if(ans < set.size()) ans = set.size();
+			for(Integer key : eated.keySet()){
+				if(eated.get(key) >= 1) {
+					cnt ++;
+				}
+			}
+			if(ans < cnt) ans = cnt;
 		}
 		return ans;
 	}
 }
+
