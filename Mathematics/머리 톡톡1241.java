936c0c588c8a18ba0643c2e4a826feaa5837a08e
KwakMJ <kwak1126mj@gmail.com>
2024-09-14 15:38:41 +0900

    ✨feat: [골드5] 머리 톡톡 1241번 : 수학, 정수론, 소수 판정
    백준 1241번 : 머리 톡톡 (골드5)
    알고리즘 분류 : 수학, 정수론, 소수 판정
    풀이 : https://cuckoobird.tistory.com/215
    결과 : 맞았습니다 (메모리 71732KB | 시간 1236ms)

---
+++ Mathematics/머리 톡톡1241.java
@@ -0,0 +1,50 @@
+/*
+백준 1241번 : 머리 톡톡 (골드5)
+알고리즘 분류 : 수학, 정수론, 소수 판정
+풀이 : https://cuckoobird.tistory.com/215
+결과 : 맞았습니다 (메모리 71732KB | 시간 1236ms)
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.valueOf(br.readLine());
+		int[] arr = new int[n+1];
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for(int i=1; i<=n; i++) {
+			int curr = Integer.valueOf(br.readLine());
+			arr[i] = curr;
+			if(map.containsKey(curr)) map.put(curr, map.get(curr)+1);
+			else map.put(curr, 1);
+		}
+		
+		int[] result = new int[1_000_001];
+		for(int key: map.keySet()) {
+			int answer = 0;
+			
+			if(key <= 1) answer = map.get(key) - 1; 
+			else {
+				for(int i=1; i<=Math.sqrt(key); i++) {
+					
+					if(key % i == 0) {
+						if(map.containsKey(i)) answer += map.get(i);
+						if(map.containsKey(key/i)) {
+							answer += map.get(key/i);
+							if(key/i == key) answer --;
+							if(key/i == i) answer -= map.get(i);
+						}
+					}
+				}
+			}
+			result[key] = answer;
+		}
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(result[arr[i]]);
+		}
+	}
+
+}
